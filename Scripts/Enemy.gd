0c1c8cade67bade15c2e9da4a93c8a6b3e73b180
MichaelOdermatt <michaelodermatt@outlook.com>
2021-12-20 08:17:55 -0800

    enemy can switch between states

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -2,19 +2,20 @@ extends KinematicBody
 
 var path = []
 var path_node = 0
-var wander_pos_index : int
-var path_waypoint
+var wander_pos_index : int = 0
+var wander_pos_path = null
+var reached_destination_threshold = 1.2
 
 var speed = 7
 enum {
-	WAITING,
-	MOVING,
-	ATTACKING
+	WAIT,
+	WANDER,
+	ATTACK
 }
 
 onready var nav = $"../Map/Navigation"
 onready var player = $"../Player"
-onready var move_to_player_timer = $"UpdatePathToPlayerTimer"
+onready var path_to_player_timer = $"PathToPlayerTimer"
 onready var state = null
 onready var wander_pos = [
 	$"WanderPos1".global_transform.origin,
@@ -28,16 +29,18 @@ onready var rng = RandomNumberGenerator.new()
 # stop the UpdatePathToPlayerTimer and randomy set the waypoint to wander
 
 func _ready():
-	var wander_pos_index = 0
-	var path_waypoint = null
-	wait()
+	wander()
 
 func _physics_process(delta):
 	match state:
-		ATTACKING, MOVING:
+		ATTACK:
 			move_along_path()
-		WAITING:
+		WANDER:
+			move_along_path()
+			wait_if_destination_reached()
+		WAIT:
 			pass
+			
 
 func move_along_path():
 	if path_node < path.size():
@@ -54,42 +57,41 @@ func set_random_waypoint():
 	# ensure the newly generated waypoint is not the current waypoint
 	if new_wander_pos_index == wander_pos_index:
 		new_wander_pos_index += 1 
-		if new_wander_pos_index > 3:
-			new_wander_pos_index = 0
+	if new_wander_pos_index > 3:
+		new_wander_pos_index = 0
+		
 	wander_pos_index = new_wander_pos_index
-	
-	path_waypoint = wander_pos[wander_pos_index]
+	wander_pos_path = wander_pos[wander_pos_index]
 
 func move_to(target_pos):
 	path = nav.get_simple_path(global_transform.origin, target_pos)
 	path_node = 0
-
-func update_waypoint_from_state():
-	match state:
-		ATTACKING:
-			path_waypoint = player.global_transform.origin
-		MOVING:
-			set_random_waypoint()
-		WAITING:
-			path_waypoint = global_transform.origin
-
-func _on_UpdatePathToPlayerTimer_timeout():
+	
+func _on_PathToPlayerTimer_timeout():
 	move_to(player.global_transform.origin)
 
 func attack():
-	move_to_player_timer.start()
-	state = ATTACKING
+	if state != ATTACK:
+		path_to_player_timer.start()
+		state = ATTACK
 	
 func wander():
-	if !move_to_player_timer.is_stopped():
-		move_to_player_timer.stop()
-	set_random_waypoint()
-	move_to(path_waypoint)
-	state = MOVING
+	if state != WANDER:
+		stop_path_to_player_timer()
+		set_random_waypoint()
+		move_to(wander_pos_path)
+		state = WANDER
 
 func wait():
-	if !move_to_player_timer.is_stopped():
-		move_to_player_timer.stop()
-	path_waypoint = global_transform.origin
-	move_to(path_waypoint)
-	state = WAITING
+	if state != WAIT:
+		stop_path_to_player_timer()
+		state = WAIT
+
+func stop_path_to_player_timer():
+	if !path_to_player_timer.is_stopped():
+		path_to_player_timer.stop()
+
+# check if enemy has reached its destination, if so then wait
+func wait_if_destination_reached():
+	if global_transform.origin.distance_to(wander_pos_path) <= reached_destination_threshold:
+		state = WAIT
